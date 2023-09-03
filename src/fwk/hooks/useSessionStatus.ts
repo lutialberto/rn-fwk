e5f8d91f781048805a06503c1fbdae4522c45470
Alberto Luti <betomdq.93@gmail.com>
2023-09-03 20:30:11 -0300

    Feat: useSessionStatus hook

---
+++ src/fwk/hooks/useSessionStatus.ts
@@ -0,0 +1,55 @@
+import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware';
+import { getItemAsync,deleteItemAsync,setItemAsync} from 'expo-secure-store';
+
+type BaseSessionData<T> = {
+  customData: { content: T };
+  baseData: {
+    isSignedIn?: boolean;
+  };
+  login: (customData: T) => void;
+  logout: () => void;
+};
+
+const INITIAL_CUSTOM_DATA: BaseSessionData<{}>["customData"] = {
+  content: {},
+};
+
+const useDataImpl = create<BaseSessionData<unknown>>()(
+  persist(
+  (set) => ({
+    customData: INITIAL_CUSTOM_DATA,
+    baseData: {
+    isSignedIn: undefined,
+    },
+    login: (customData: unknown) => 
+      set(() => ({
+        customData: { content: customData },
+        baseData: {
+          isSignedIn: true
+        }
+      })),
+    logout: () => 
+      set(() => ({
+        customData: INITIAL_CUSTOM_DATA,
+        baseData: {
+          isSignedIn: false
+        }
+      })),
+  }),
+    {
+      name: `session-status-storage`,
+      storage: createJSONStorage(() => ({
+        getItem: name => getItemAsync(name),
+        removeItem: name => deleteItemAsync(name),
+        setItem: (name: string, value: string) => setItemAsync(name, value),
+      })),
+      version: 1,
+    },
+  )
+);
+
+export const useSessionStatus = useDataImpl as {
+  <T>(): BaseSessionData<T>;
+  <T, U>(selector: (s: BaseSessionData<T>) => U): U;
+};
