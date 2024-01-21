0f5f5c2b85e63ebbb6f84e14e9442432c0f95dd5
Alberto Luti <betomdq.93@gmail.com>
2024-01-21 14:41:10 -0300

    Update: apply sectionList to toastScreen view

--- src/screens/home/tabs/moreTab/screens/toast/ToastScreen.tsx
+++ src/screens/home/tabs/moreTab/screens/toast/ToastScreen.tsx
@@ -4,6 +4,7 @@ import ScreenContainerApp from 'components/containers/screenContainer/ScreenCont
 import ButtonApp from 'components/buttons/buttonApp/ButtonApp';
 import {useToastApp} from 'hooks/toast/useToastApp';
 import TextApp from 'components/texts/textApp/TextApp';
+import SectionList, {SectionListProps} from 'components/containers/sectionList/SectionList';
 
 const ToastScreen = () => {
   const {showToast} = useToastApp();
@@ -14,38 +15,70 @@ const ToastScreen = () => {
       type: 'success',
     });
   };
+
+  const list: SectionListProps = {
+    sections: [
+      {
+        name: 'Tipos de tostadas',
+        items: [
+          {
+            name: 'Éxito',
+            onPress: () => showToast({message: 'Éxito', type: 'success'}),
+          },
+          {
+            name: 'Error',
+            onPress: () => showToast({message: 'Error', type: 'error'}),
+          },
+          {
+            name: 'Info',
+            onPress: () => showToast({message: 'Info', type: 'info'}),
+          },
+          {
+            name: 'Alerta',
+            onPress: () => showToast({message: 'Alerta', type: 'warning'}),
+          },
+        ],
+      },
+      {
+        name: 'Duración',
+        items: [
+          {
+            name: '0.5 segundos',
+            onPress: () => showToast({message: '0.5 segundos', durationMiliseconds: 500}),
+          },
+          {
+            name: '2 segundos',
+            onPress: () => showToast({message: '2 segundos', durationMiliseconds: 2000}),
+          },
+          {
+            name: '5 segundos',
+            onPress: () => showToast({message: '5 segundos', durationMiliseconds: 5000}),
+          },
+        ],
+      },
+      {
+        name: 'Posición desde el tope',
+        items: [
+          {
+            name: 'A 10 del tope',
+            onPress: () => showToast({message: 'A 10 del tope', positionFromTop: 10}),
+          },
+          {
+            name: 'En el medio',
+            onPress: () => showToast({message: 'En el medio', positionFromTop: 0}),
+          },
+          {
+            name: 'Abajo',
+            onPress: () => showToast({message: 'Abajo'}),
+          },
+        ],
+      },
+    ],
+  };
+
   return (
-    <ScreenContainerApp showBackButton title="Tostadas" style={{gap: 10}}>
-      <TextApp>Tipos de tostadas</TextApp>
-      <ButtonApp label={'Éxito'} onPress={() => showToast({message: 'Éxito', type: 'success'})} />
-      <ButtonApp label={'Error'} onPress={() => showToast({message: 'Error', type: 'error'})} />
-      <ButtonApp label={'Info'} onPress={() => showToast({message: 'Info', type: 'info'})} />
-      <ButtonApp label={'Alerta'} onPress={() => showToast({message: 'Alerta', type: 'warning'})} />
-
-      <TextApp>Duración</TextApp>
-      <ButtonApp
-        label={'0.5 segundos'}
-        onPress={() => showToast({message: '0.5 segundos', durationMiliseconds: 500})}
-      />
-      <ButtonApp
-        label={'2 segundos'}
-        onPress={() => showToast({message: '2 segundos', durationMiliseconds: 2000})}
-      />
-      <ButtonApp
-        label={'5 segundos'}
-        onPress={() => showToast({message: '5 segundos', durationMiliseconds: 5000})}
-      />
-
-      <TextApp>Posición desde el tope</TextApp>
-      <ButtonApp
-        label={'A 10 del tope'}
-        onPress={() => showToast({message: 'A 10 del tope', positionFromTop: 10})}
-      />
-      <ButtonApp
-        label={'En el medio'}
-        onPress={() => showToast({message: 'En el medio', positionFromTop: 0})}
-      />
-      <ButtonApp label={'Abajo'} onPress={() => showToast({message: 'Abajo'})} />
+    <ScreenContainerApp showBackButton title="Tostadas">
+      <SectionList sections={list.sections} />
     </ScreenContainerApp>
   );
 };
