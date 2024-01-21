d583a17ce22b98ab93bf2c23b2a5f16ea64464ec
Alberto Luti <betomdq.93@gmail.com>
2024-01-21 14:17:50 -0300

    Update: replace button list with sectionList implementation

--- src/screens/home/tabs/formsTab/FormsTab.tsx
+++ src/screens/home/tabs/formsTab/FormsTab.tsx
@@ -3,37 +3,15 @@ import React from 'react';
 import ScreenContainerApp from 'components/containers/screenContainer/ScreenContainerApp';
 import TextApp from 'components/texts/textApp/TextApp';
 import {useTextStyles} from 'components/texts/hooks/useTextStyles';
-import {useNavigation} from '@react-navigation/native';
-import {MainScreenNavigationType} from 'navigation/MainStack';
 import AvailableForms from './components/availableForms/AvailableForms';
 
 const FormsTab = () => {
   const {styles: hookStyles} = useTextStyles();
-  const navigator = useNavigation<MainScreenNavigationType>();
-
-  const forms = [
-    {
-      label: 'Login',
-      onPress: () => navigator.navigate('Forms/Login'),
-    },
-    {
-      label: 'Register',
-      onPress: () => navigator.navigate('Forms/Register'),
-    },
-    {
-      label: 'Tipos de input',
-      onPress: () => navigator.navigate('Forms/InputTypes'),
-    },
-    {
-      label: 'Validaciones',
-      onPress: () => navigator.navigate('Forms/Validations'),
-    },
-  ];
 
   return (
     <ScreenContainerApp style={styles.screenContainer}>
       <TextApp style={[hookStyles.screenTitle, styles.screenTitle]}>Formularios</TextApp>
-      <AvailableForms forms={forms} />
+      <AvailableForms />
     </ScreenContainerApp>
   );
 };
--- src/screens/home/tabs/formsTab/components/availableForms/AvailableForms.tsx
+++ src/screens/home/tabs/formsTab/components/availableForms/AvailableForms.tsx
@@ -1,21 +1,45 @@
 import {ScrollView, StyleSheet} from 'react-native';
 import React from 'react';
 import ButtonApp from 'components/buttons/buttonApp/ButtonApp';
+import {useNavigation} from '@react-navigation/native';
+import {MainScreenNavigationType} from 'navigation/MainStack';
+import SectionList, {SectionListProps} from 'components/containers/sectionList/SectionList';
 
-interface Props {
-  forms: {
-    label: string;
-    onPress: () => void;
-  }[];
-}
-const AvailableForms = ({forms}: Props) => {
-  return (
-    <ScrollView contentContainerStyle={styles.buttonsContainer}>
-      {forms.map(({label, onPress}) => (
-        <ButtonApp key={label} onPress={onPress} label={label} />
-      ))}
-    </ScrollView>
-  );
+const AvailableForms = () => {
+  const navigator = useNavigation<MainScreenNavigationType>();
+
+  const list: SectionListProps = {
+    sections: [
+      {
+        name: 'Prototípicos',
+        items: [
+          {
+            name: 'Login',
+            onPress: () => navigator.navigate('Forms/Login'),
+          },
+          {
+            name: 'Register',
+            onPress: () => navigator.navigate('Forms/Register'),
+          },
+        ],
+      },
+      {
+        name: 'Ejemplos prácticos',
+        items: [
+          {
+            name: 'Tipos de input',
+            onPress: () => navigator.navigate('Forms/InputTypes'),
+          },
+          {
+            name: 'Validaciones',
+            onPress: () => navigator.navigate('Forms/Validations'),
+          },
+        ],
+      },
+    ],
+  };
+
+  return <SectionList sections={list.sections} />;
 };
 
 export default AvailableForms;
