a517a06366f50af6683a4d8b4aa738d4241ac98a
Alberto Luti <betomdq.93@gmail.com>
2024-03-28 18:09:21 -0300

    Refactor: move sections of MoreTab to a customHook

--- src/screens/home/tabs/moreTab/MoreTab.tsx
+++ src/screens/home/tabs/moreTab/MoreTab.tsx
@@ -2,66 +2,12 @@ import React from 'react';
 import ScreenContainerApp from 'components/containers/screenContainer/ScreenContainerApp';
 import {MainScreenNavigationType} from 'navigation/MainStack';
 import {useNavigation} from '@react-navigation/native';
-import SectionList, {SectionListSectionsProps} from 'components/containers/sectionList/SectionList';
+import SectionList from 'components/containers/sectionList/SectionList';
+import {useMoreTabSections} from './hooks/useMoreTabSections';
 
 const MoreTab = () => {
-  const sections: SectionListSectionsProps[] = [
-    {
-      name: 'Assets',
-      items: [{name: 'Íconos', onPress: () => navigator.navigate('Assets/Icon')}],
-    },
-    {
-      name: 'Gráficos',
-      items: [
-        {name: 'Línea', onPress: () => navigator.navigate('Charts/Line')},
-        {name: 'Progreso', onPress: () => navigator.navigate('Charts/Progress')},
-        {name: 'Barra', onPress: () => navigator.navigate('Charts/Bar')},
-        {name: 'Barras Apiladas', onPress: () => navigator.navigate('Charts/StackedBar')},
-      ],
-    },
-    {
-      name: 'Componentes',
-      items: [
-        {name: 'Texto', onPress: () => navigator.navigate('Components/Text')},
-        {name: 'Botón', onPress: () => navigator.navigate('Components/Button')},
-        {name: 'Spinner', onPress: () => navigator.navigate('Components/Spinner')},
-        {name: 'Card', onPress: () => navigator.navigate('Components/Card')},
-        {name: 'Sección', onPress: () => navigator.navigate('Components/SectionList')},
-        {name: 'Solapa', onPress: () => navigator.navigate('Components/Tab')},
-        {name: 'Modal', onPress: () => navigator.navigate('Components/Modal')},
-        {name: 'Carrusel', onPress: () => navigator.navigate('Components/Carrousel')},
-        {name: 'Porta papeles', onPress: () => navigator.navigate('Components/Clipboard')},
-      ],
-    },
-    {
-      name: 'Hooks',
-      items: [
-        {name: 'Tostada', onPress: () => navigator.navigate('Hooks/Toast')},
-        {name: 'Manejo de sesión', onPress: () => navigator.navigate('Hooks/SessionStatus')},
-        {name: 'Chequeo internet', onPress: () => navigator.navigate('Hooks/NetInfo')},
-        {
-          name: 'Autenticación biométrica',
-          onPress: () => navigator.navigate('Hooks/LocalAuthentication'),
-        },
-      ],
-    },
-    {
-      name: 'Snippets',
-      items: [
-        {name: 'Componentes', onPress: () => navigator.navigate('Snippets/Components')},
-        {name: 'Gráficos', onPress: () => navigator.navigate('Snippets/Charts')},
-        {name: 'Formularios', onPress: () => navigator.navigate('Snippets/Forms')},
-        {name: 'Hooks', onPress: () => navigator.navigate('Snippets/Hooks')},
-        {name: 'Navegación', onPress: () => navigator.navigate('Snippets/Navigation')},
-        {name: 'Servicios', onPress: () => navigator.navigate('Snippets/Service')},
-        {name: 'Solapas', onPress: () => navigator.navigate('Snippets/Tab')},
-        {name: 'Debug', onPress: () => navigator.navigate('Snippets/Debug')},
-        {name: 'Generales', onPress: () => navigator.navigate('Snippets/General')},
-      ],
-    },
-  ];
-
   const navigator = useNavigation<MainScreenNavigationType>();
+  const sections = useMoreTabSections(navigator);
 
   return (
     <ScreenContainerApp title="Componentes genéricos reutilizables">
