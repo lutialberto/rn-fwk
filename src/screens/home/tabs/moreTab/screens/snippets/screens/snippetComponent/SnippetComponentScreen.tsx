4cb89ff427459b352ec494b2dc2c33bc5728ed2b
Alberto Luti <betomdq.93@gmail.com>
2024-03-28 20:10:17 -0300

    Feat: added snippetComponent prop to snippets of SnippetComponentScreen

--- src/screens/home/tabs/moreTab/screens/snippets/screens/snippetComponent/SnippetComponentScreen.tsx
+++ src/screens/home/tabs/moreTab/screens/snippets/screens/snippetComponent/SnippetComponentScreen.tsx
@@ -1,15 +1,53 @@
+import {Text, View} from 'react-native';
 import React from 'react';
 import SnippetScreenTemplate from '../../components/snippetScreenTemplate/SnippetScreenTemplate';
+import ButtonApp from 'components/buttons/buttonApp/ButtonApp';
+import TextApp from 'components/texts/textApp/TextApp';
+import ModalApp from 'components/containers/modals/modal/ModalApp';
+import SpinnerApp from 'components/containers/loading/spinner/SpinnerApp';
+import CarrouselApp from 'components/containers/carrousel/CarrouselApp';
+import TextCopyToClipboardApp from 'components/texts/textCopyToClipboard/TextCopyToClipboardApp';
 
 const SnippetComponentScreen = () => {
   const snippets = [
-    {label: 'Button', prefix: 'appButton'},
-    {label: 'Text', prefix: 'appText'},
-    {label: 'Modal', prefix: 'appModal'},
+    {
+      label: 'Button',
+      prefix: 'appButton',
+      snippetComponent: <ButtonApp label={'Button'} onPress={() => {}} />,
+    },
+    {label: 'Text', prefix: 'appText', snippetComponent: <TextApp>{'Text'}</TextApp>},
+    {
+      label: 'Modal',
+      prefix: 'appModal',
+    },
     {label: 'BottomSheet', prefix: 'appBottomSheet'},
-    {label: 'Spinner', prefix: 'appSpinner'},
-    {label: 'Carrusel', prefix: 'appCarrousel'},
-    {label: 'Porta papeles', prefix: 'appClipboard'},
+    {
+      label: 'Spinner',
+      prefix: 'appSpinner',
+      snippetComponent: (
+        <SpinnerApp visible={true}>
+          <Text>Spinner Result</Text>
+        </SpinnerApp>
+      ),
+    },
+    {
+      label: 'Carrusel',
+      prefix: 'appCarrousel',
+      snippetComponent: (
+        <CarrouselApp
+          containerStyle={{
+            height: 200, //usar este
+            //flex: 1,//o este
+          }}
+          items={[<TextApp>Page 1 </TextApp>, <TextApp>Page 2</TextApp>, <TextApp>Page 3</TextApp>]}
+        />
+      ),
+    },
+    {
+      label: 'Porta papeles',
+      prefix: 'appClipboard',
+      snippetComponent: <TextCopyToClipboardApp textToCopy={'texto...'} />,
+    },
   ];
   return <SnippetScreenTemplate title="Snippet de componentes" snippetList={snippets} />;
 };
